ee98039dfabf2e17726b04b2535f8e6ac0d95e36
ZAHIDUL_ALAM <zahidpranta5@gmail.com>
2021-09-09 20:45:39 +0600

    UUID added as a matchId

--- players/models.py
+++ players/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid,time
 #from django.utils.translation import activate
 
 # Create your models here.
@@ -7,16 +8,26 @@ mapchoice=(("BERMUDA","KALAHARI"),("BERMUDA","BERMUDA"))
 gamechoice=(("CLASH_SQUAD","CLASH SQUAD"),("CLASSIC","CLASSIC"))
 ammuchoice=(("limited","LIMITED"),("unlimited","UNLIMITED"))
 gametime=(("10am","10AM"),("2am","2AM"))
+entryfeechoice=((15.0,15),(50.0,50),(100.0,100.0))
+
+
+UUID=uuid.uuid1(node=None,clock_seq=None)
 
 class Matches(models.Model):
+    #gameid =models.UUIDField(default=str(uuid.uuid4), editable=False)
+    #gameid=models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4)
+    Matchid=models.CharField(max_length=100, blank=True, unique=True, default=UUID.time)
     GameMood=models.CharField(max_length=120,choices=gamechoice,default="CLASSIC")
     map=models.CharField(max_length=50,choices=mapchoice,default="BERMUDA")
     ammu=models.CharField(max_length=20, choices=ammuchoice ,default="Limited")
     time=models.CharField(max_length=20, choices=gametime ,default="00PM")
+    entryfee=models.FloatField(choices=entryfeechoice,default=15.0)
+    
+    
    
 
     def __str__(self):
-         return self.GameMood
+         return self.GameMood + " "+ self.Matchid
 
 
 
